14764337aeadfb8b5b6931219aac1eb234de1bbb
Kevin Mejia <kwmejia9@gmail.com>
2023-02-26 20:25:21 -0500

    dev-05-backend-servidor-de-archivos

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 import swaggerUI from 'swagger-ui-express';
 import swaggerJsDoc from 'swagger-jsdoc';
 import emailsVeterinarian from './src/routes/emailsVeterinarian.routes.js'
@@ -10,6 +12,7 @@ import { dbConfig } from './src/config/connection.js';
 
 //settings
 const app = express();
+const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
 
 //swagger
 const swaggerjsDoc = swaggerJsDoc(optionsSwagger);
@@ -19,6 +22,8 @@ app.use(express.json())
 app.use(cors());
 app.use(conn(mysql, dbConfig));
 app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerjsDoc));
+app.use('/public', express.static(join(CURRENT_DIR, "./uploads")));
+
 
 //Routes
 app.use('/api/emailsVeterinarian', emailsVeterinarian);
